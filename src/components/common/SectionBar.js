2af1cf8a7d29e3422f857f5cbb913f7054a51bd4
happyrina <riniversal@gmail.com>
2023-09-07 00:39:52 +0900

    "SectionBar만들기"

---
+++ src/components/common/SectionBar.js
@@ -0,0 +1,15 @@
+function SectionBar() {
+  return (
+    <div
+      style={{
+        display: 'block',
+        width: '100%',
+        height: '10px',
+        backgroundColor: 'rgb(239, 239, 239)',
+        margin: '40px 0px',
+      }}
+    ></div>
+  )
+}
+
+export default SectionBar
